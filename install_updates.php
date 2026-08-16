c13cbe8a1aec453992542a43c897fcb2219f0cc8
Ben <hostellerie.org@gmail.com>
2026-08-16 08:35:24 +0200

    Add Analytics 1.1.3 configuration upgrade

--- install_updates.php
+++ install_updates.php
@@ -2,7 +2,7 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | analytics plugin 1.1.2                                                    |
+// | analytics plugin 1.1.3                                                    |
 // +---------------------------------------------------------------------------+
 // | install_updates.php                                                       |
 // |                                                                           |
@@ -30,62 +30,52 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-
 if (stripos($_SERVER['PHP_SELF'], basename(__FILE__)) !== false) {
     die('This file can not be used on its own!');
 }
 
-/**
- * Update Configuration Values for analytics 1.1.1
- * 
- * In this version, we introduced the native Geeklog Configuration API.
- * This function initializes the structure during an upgrade from a version
- * that did not have it.
- *
- * @return boolean true on success
- */
 function analytics_update_ConfValues_1_1_1()
 {
     global $_CONF, $_PI_CONF;
 
     $c = config::get_instance();
-
     require_once $_CONF['path'] . 'plugins/analytics/install_defaults.php';
-    
-    // Group 'analytics'
+
     if (!$c->group_exists('analytics')) {
         $c->add('sg_main', NULL, 'subgroup', 0, 0, NULL, 0, true, 'analytics', 0);
         $c->add('tab_main', NULL, 'tab', 0, 0, NULL, 0, true, 'analytics', 0);
         $c->add('fs_main', NULL, 'fieldset', 0, 0, NULL, 0, true, 'analytics', 0);
-        
-        // Define default values if $_PI_CONF is not populated yet
-        $ga_code = isset($_PI_CONF['analytics']['ga_code']) ? $_PI_CONF['analytics']['ga_code'] : '';
-        $client_id = isset($_PI_CONF['analytics']['client_id']) ? $_PI_CONF['analytics']['client_id'] : '';
-        
-        $c->add('ga_code', $ga_code, 'text', 0, 0, NULL, 10, true, 'analytics', 0);
-        $c->add('client_id', $client_id, 'text', 0, 0, NULL, 20, true, 'analytics', 0);
+        $c->add('ga_code', isset($_PI_CONF['analytics']['ga_code']) ? $_PI_CONF['analytics']['ga_code'] : '', 'text', 0, 0, NULL, 10, true, 'analytics', 0);
+        $c->add('client_id', isset($_PI_CONF['analytics']['client_id']) ? $_PI_CONF['analytics']['client_id'] : '', 'text', 0, 0, NULL, 20, true, 'analytics', 0);
     }
 
     return true;
 }
 
-/**
- * Update Configuration Values for analytics 1.1.2
- *
- * @return boolean true on success
- */
 function analytics_update_ConfValues_1_1_2()
 {
-    global $_CONF;
     $c = config::get_instance();
-
-    // Ensure previous updates are applied
     analytics_update_ConfValues_1_1_1();
 
     if ($c->group_exists('analytics')) {
         $analyticsConfig = $c->get_config('analytics');
         if (is_array($analyticsConfig) && !isset($analyticsConfig['property_id'])) {
             $c->add('property_id', '', 'text', 0, 0, NULL, 15, true, 'analytics', 0);
+        }
+    }
+
+    return true;
+}
+
+function analytics_update_ConfValues_1_1_3()
+{
+    $c = config::get_instance();
+    analytics_update_ConfValues_1_1_2();
+
+    if ($c->group_exists('analytics')) {
+        $analyticsConfig = $c->get_config('analytics');
+        if (is_array($analyticsConfig) && !isset($analyticsConfig['hostname'])) {
+            $c->add('hostname', '', 'text', 0, 0, NULL, 25, true, 'analytics', 0);
         }
     }
 
